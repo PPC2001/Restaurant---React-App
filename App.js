948cdf8ab6889d04e951a11366883d5a37ee4563
Pratik Chavan <pratik2612001@gmail.com>
2023-01-28 13:08:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Restaurant from './component/Basics/Restaurant';
+
+const App = () => {
+  return (
+    <>
+        <Restaurant></Restaurant>
+    </>
+    )
+}
+
+export default App;
+
+
